3fd6da43e1efa23ce8646088f5bf3f61ea281e19
devPedroAugusto <desenvolvedor.pedroaugusto@gmail.com>
2023-11-27 13:48:29 -0300

    Create Pokedex Interface

--- Pokedex-Code_Angular/src/app/page/page.component.ts
+++ Pokedex-Code_Angular/src/app/page/page.component.ts
@@ -4,11 +4,12 @@ import { SmallCardComponent } from './small-card/small-card.component';
 import { BigCardComponent } from './big-card/big-card.component';
 import { LoadingCardComponent } from './loading-card/loading-card.component';
 import { MoreCardComponent } from './more-card/more-card.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [CommonModule, SmallCardComponent, BigCardComponent, LoadingCardComponent, MoreCardComponent],
+  imports: [CommonModule, PokedexComponent, SmallCardComponent, BigCardComponent, LoadingCardComponent, MoreCardComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
